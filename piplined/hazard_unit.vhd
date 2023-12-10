6dc1450f54ab46d746f77ec6c5dd2a244ff2443e
khaled shaaban <khalid.shapan.mohamed@gmail.com>
2023-12-10 14:29:25 +0200

    hazard unit modified

--- piplined/hazard_unit.vhd
+++ piplined/hazard_unit.vhd
@@ -11,7 +11,7 @@ ENTITY hazard_unit IS
     i_RtD : IN STD_LOGIC_VECTOR (4 DOWNTO 0);
     i_branchD : IN STD_LOGIC;
     o_forwardAD : OUT STD_LOGIC;
-    o_forwardAB : OUT STD_LOGIC;
+    o_forwardBD : OUT STD_LOGIC;
 
     o_flushE : OUT STD_LOGIC;
     i_RsE : IN STD_LOGIC_VECTOR (4 DOWNTO 0);
@@ -35,7 +35,7 @@ END hazard_unit;
 ARCHITECTURE RTL OF hazard_unit IS
 
   SIGNAL s_lw_stall : STD_LOGIC := '0';
-  SIGNAL s_branch_stall := STD_LOGIC := '0';
+  SIGNAL s_branch_stall : STD_LOGIC := '0';
 BEGIN
   -- introduce forwarding
   RAW_srcA_hazard : PROCESS (i_RsE, i_RdM, i_RdW, i_reg_writeM, i_reg_writeW) BEGIN
@@ -67,18 +67,21 @@ BEGIN
   END PROCESS;
 
   beqz_hazard : PROCESS (i_RsD, i_RtD, i_RdM, i_reg_writeM) BEGIN
-    IF ((i_RsD /= 0) AND (i_RsD = i_Rdm) AND (i_reg_writeM = '1')) THEN
+    -- data is in memory stage, forwarding 
+    IF ((i_RsD /= 0) AND (i_RsD = i_RdM) AND (i_reg_writeM = '1')) THEN
       o_forwardAD <= '1';
     ELSE
       o_forwardAD <= '0';
     END IF;
 
-    IF ((i_RtD /= 0) AND (i_RtD = i_Rdm) AND (i_reg_writeM = '1')) THEN
+    -- data is in memory stage, forwarding 
+    IF ((i_RtD /= 0) AND (i_RtD = i_RdM) AND (i_reg_writeM = '1')) THEN
       o_forwardBD <= '1';
     ELSE
       o_forwardBD <= '0';
     END IF;
 
+    -- data is in execute stage or lw in mem stage , stalling
     IF ((i_branchD = '1') AND (i_reg_writeE = '1') AND ((i_RdE = i_RsD) OR (i_RdE = i_RtD))) OR
       ((i_branchD = '1') AND (i_mem_2_regM = '1') AND ((i_RdM = i_RsD) OR (i_RdM = i_RtD))) THEN
       s_branch_stall <= '1';
@@ -86,5 +89,12 @@ BEGIN
       s_branch_stall <= '0';
     END IF;
   END PROCESS;
+
+  o_stallF <= '1' WHEN (s_branch_stall = '1') OR (s_lw_stall = '1') ELSE
+    '0';
+  o_stallD <= '1' WHEN (s_branch_stall = '1') OR (s_lw_stall = '1') ELSE
+    '0';
+  o_flushE <= '1' WHEN (s_branch_stall = '1') OR (s_lw_stall = '1') ELSE
+    '0';
 
 END ARCHITECTURE RTL;
